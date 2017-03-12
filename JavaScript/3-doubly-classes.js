649cca431eb69d1254c461693ab77f6ce01bcfb1
Evgeniia Zubrich <evg.zubrich@gmail.com>
2017-03-12 20:57:07 +0200

    code style is improved

--- JavaScript/3-doubly-classes.js
+++ JavaScript/3-doubly-classes.js
@@ -1,39 +1,47 @@
+/*eslint linebreak-style: ["error", "windows"]*/
 'use strict';
-
-function LinkedList() {
-  this.first = null;
-  this.last = null;
-  this.length = 0;
-  
-  this.push = function(data) {
-    const newNode = new Node(this, data);
-    let currentNode = this.last;
-    newNode.prev = currentNode;
-    if (this.length === 0) this.first = newNode;
-    else currentNode.next = newNode;
-    this.last = newNode;
-    this.length++;
-    return newNode;
-  };
-  
-  this.pop = function() {
-  if (this.length > 0) {
-    const node = this.last;
-    this.last = node.prev;
-    if (node){
-    node.next = null;
-    node.list = null;
-    node.prev = null; }
-    this.length--;
-    return node.data;
-  };
- 
-  
-}
-
 function Node(list, data) {
   this.list = list;
   this.data = data;
   this.prev = null;
   this.next = null;
 }
+
+function LinkedList() {
+  this.first = null;
+  this.last = null;
+  this.length = 0;
+  this.push = function(data) {
+    let newNode = new Node(this, data);
+    const currentNode = this.last;
+    newNode.prev = currentNode;
+    if (this.length === 0) this.first = newNode;
+    else currentNode.next = newNode;
+    this.last = newNode;
+    this.length = this.length + 1;
+    return newNode;
+  };
+  this.pop = function() {
+    if (this.length > 0) {
+      const node = this.last;
+      this.last = node.prev;
+      if (node) {
+        node.next = null;
+        node.prev = null;
+        node.list = null;
+      }
+      this.length =  this.length - 1;
+      return node.data;
+    }
+  };
+}
+
+const list = new LinkedList();
+list.push({ name: 'first' });
+list.push({ name: 'second' });
+list.push({ name: 'third' });
+
+console.dir(list.pop());
+console.dir(list.pop());
+console.dir(list.pop());
+console.dir(list.pop());

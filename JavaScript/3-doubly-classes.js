f1a43b361e4743ce7138634550ce2b1500db8e8c
Evgeniia Zubrich <evg.zubrich@gmail.com>
2017-03-12 20:54:21 +0200

    added push & pop

--- JavaScript/3-doubly-classes.js
+++ JavaScript/3-doubly-classes.js
@@ -4,6 +4,31 @@ function LinkedList() {
   this.first = null;
   this.last = null;
   this.length = 0;
+  
+  this.push = function(data) {
+    const newNode = new Node(this, data);
+    let currentNode = this.last;
+    newNode.prev = currentNode;
+    if (this.length === 0) this.first = newNode;
+    else currentNode.next = newNode;
+    this.last = newNode;
+    this.length++;
+    return newNode;
+  };
+  
+  this.pop = function() {
+  if (this.length > 0) {
+    const node = this.last;
+    this.last = node.prev;
+    if (node){
+    node.next = null;
+    node.list = null;
+    node.prev = null; }
+    this.length--;
+    return node.data;
+  };
+ 
+  
 }
 
 function Node(list, data) {
